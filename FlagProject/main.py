163f6a4d77a63d6a0c6f19a210175bedf171e966
Matan Coiffman <coiffmanmatan@gmail.com>
2024-09-10 16:48:17 +0300

    Move_Player

--- FlagProject/main.py
+++ FlagProject/main.py
@@ -8,12 +8,19 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 background = pygame.color.Color('dark green')
 run = True
-
+# Calculate cell size
+cell_width_jump = SCREEN_WIDTH // 50
+cell_height_jump = SCREEN_HEIGHT // 25
 grass_image = pygame.image.load(r'bin/grass.png')
-new_width, new_height = 50, 50  # Adjust the desired size here
-grass_image = pygame.transform.scale(grass_image, (new_width, new_height))
-
+matrix = []
 grass_images = []
+grass_image = pygame.transform.scale(grass_image, (50, 50))
+
+for row in range(SCREEN_HEIGHT // cell_height_jump):
+    matrix_row = []
+    for col in range(SCREEN_WIDTH // cell_width_jump):
+        matrix_row.append("0")
+    matrix.append(matrix_row)
 
 for i in range(20):
     x = random.randint(0, SCREEN_WIDTH - grass_image.get_width())
